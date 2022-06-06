93f5ec59ab9c875f0c1dfc373fafe00f7bc0cfcc
moparinag <moparinag.b@gmail.com>
2022-06-06 18:36:49 +0530

    First program

---
+++ Java Programs/swapusingvar.java
@@ -0,0 +1,13 @@
+class swapusingvar
+{
+	public static void main(String[] args)
+	{
+		int a=100;
+		int b=200;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+	}
+}
