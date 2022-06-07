bb8eea7df131fb435d7c11577b897e08c372fe9f
moparinag <moparinag.b@gmail.com>
2022-06-07 20:03:46 +0530

    07-06-2022

---
+++ Java Programs/Typecasting.java
@@ -0,0 +1,13 @@
+class Typecasting
+{
+public static void main(String[] args)
+{
+	//Typecasting in java
+double d=4564;
+System.out.println("d="+d);
+int i=(int)45678899.566788909;
+System.out.println("i="+i);
+int c='c';
+System.out.println("c="+c);
+}
+}
