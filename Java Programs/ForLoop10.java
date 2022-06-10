a0b25be8552c41990555149753102feafbd2509d
moparinag <moparinag.b@gmail.com>
2022-06-10 19:55:28 +0530

    10-06-2022

---
+++ Java Programs/ForLoop10.java
@@ -0,0 +1,20 @@
+class ForLoop10
+{
+public static void main(String[] args)
+{
+java.util.Scanner scn=new java.util.Scanner(System.in);
+System.out.println("Enter n Value");
+int n=scn.nextInt();
+System.out.println("Enter a Value");
+int a=scn.nextInt();
+for(int i=1;i<=n;i++)
+{
+	for(int j=1;j<=a;j++)
+	{
+		System.out.print("*");
+	}
+	a--;
+	System.out.println();
+}
+}
+}
