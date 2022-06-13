7de3baff495ae6e70796013d01c4d053d58631dd
moparinag <moparinag.b@gmail.com>
2022-06-13 18:23:23 +0530

    13-06-2022

---
+++ Java Programs/SumOfFactorial.java
@@ -0,0 +1,31 @@
+class SumOfFactorial
+{
+public static void main(String[] args)
+{
+java.util.Scanner scn=new java.util.Scanner(System.in);
+System.out.println("Enter n Value");
+int n=scn.nextInt();
+System.out.println(SumOfFactorial(n));
+}
+
+public static int factorial(int a)
+{
+int fac=1;
+for(int i=1;i<=a;i++)
+{
+fac=fac*i;
+}
+return fac;
+}
+public static int SumOfFactorial(int a)
+{
+int sum=0;
+while(a>0)
+{
+int r=a%10;
+sum=sum+factorial(r);
+a=a/10;
+}
+return sum;
+}
+}
