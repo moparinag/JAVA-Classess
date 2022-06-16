23a30d7ba52fc4956c47150c1bb44a7e97a7db04
moparinag <moparinag.b@gmail.com>
2022-06-16 22:43:31 +0530

    16-06-2022

---
+++ JAVAPrograms/src/EclipsePrograms/Blocks1.java
@@ -0,0 +1,22 @@
+package EclipsePrograms;
+
+public class Blocks1 {
+
+	static
+	{
+		System.out.println("Block1");
+	}
+	static
+	{
+		System.out.println("Block2");
+	}
+	static
+	{
+		System.out.println("Block3");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
