b7c6306e16767749317dd3049b8b675ec0172506
moparinag <moparinag.b@gmail.com>
2022-07-05 17:23:09 +0530

    5-07-2022

---
+++ JAVAPrograms/src/EclipsePrograms/StringPrograms/StringCountCharacter.java
@@ -0,0 +1,17 @@
+package EclipsePrograms.StringPrograms;
+
+public class StringCountCharacter {
+
+	public static void main(String[] args) {
+		String s="JAVA DEVELOPER";
+		int count=count( s,'o');
+		System.out.println(count);
+		System.out.println(s.indexOf('j'));
+	}
+
+	private static int count(String s, char c) {
+		
+		return s.length()-s.replace(c+"", "").length();
+	}
+
+}
